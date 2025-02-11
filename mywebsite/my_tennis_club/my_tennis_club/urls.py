4ec48e82e2a0310084d0b130d2e28d5bab25c4d3
fhnasgf28 <farhan.assegaf19019@student.unsika.ac.id>
2025-02-11 21:48:24 +0700

    mytennis club

--- mywebsite/my_tennis_club/my_tennis_club/urls.py
+++ mywebsite/my_tennis_club/my_tennis_club/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for mywebsite project.
+URL configuration for my_tennis_club project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
@@ -15,18 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from django.http import HttpResponse
-
-
-def index(request):
-    return HttpResponse("Hello, world. You're at the mywebsite index.")
-
-def about(request):
-    return HttpResponse("Hello, world. You're at the mywebsite index.")
+from django.urls import include, path
 
 urlpatterns = [
+    path('', include('members.urls')),
     path('admin/', admin.site.urls),
 ]
-
-
