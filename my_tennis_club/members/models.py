a723fa9c2c040f3295b931cf0d7b0519398e607c
fhnasgf28 <farhan.assegaf19019@student.unsika.ac.id>
2025-02-24 20:11:56 +0700

    menambahkan field

--- my_tennis_club/members/models.py
+++ my_tennis_club/members/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Member(models.Model):
+    firstname = models.CharField(max_length=50)
+    lastname = models.CharField(max_length=50)
+    email = models.EmailField()
+    phone = models.CharField(max_length=15)
+    address = models.CharField(max_length=100)
+    city = models.CharField(max_length=50)
+    state = models.CharField(max_length=50)
+    zip_code = models.CharField(max_length=10)
+    membership_type = models.CharField(max_length=50)
