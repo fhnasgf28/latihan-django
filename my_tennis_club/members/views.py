1a4a19d02cb22f811e09245212e639fbf0249003
fhnasgf28 <farhan.assegaf19019@student.unsika.ac.id>
2025-03-08 19:00:25 +0700

    belajar membuat templating django

--- my_tennis_club/members/views.py
+++ my_tennis_club/members/views.py
@@ -12,3 +12,22 @@ def members(request):
         'mymembers': mymembers
     }
     return HttpResponse(template.render(context, request))
+
+def details(request, id):
+    mymember = Member.objects.get(id=id)
+    template = loader.get_template('details.html')
+    context = {
+        'mymember': mymember
+    }
+    return HttpResponse(template.render(context, request))
+
+def main(request):
+    template = loader.get_template('main.html')
+    return HttpResponse(template.render())
+
+def testing(request):
+    template = loader.get_template('templates.html')
+    context = {
+        'fruits': ['apple', 'banana', 'cherry']
+    }
+    return HttpResponse(template.render(context, request))
