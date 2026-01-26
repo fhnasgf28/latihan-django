4ed6e235f6dc99f4d2a03a295c4e327d58f0e128
fhnasgf28 <farhan.assegaf19019@student.unsika.ac.id>
2026-01-26 13:58:29 +0700

    latihan django vuejs

---
+++ latihan_django_vue/notes/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+class Note(models.Model):
+    title = models.CharField(max_length=120)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
---
+++ latihan_django_vue/notes/views.py
@@ -0,0 +1,4 @@
+from django.shortcuts import render
+from rest_framework.respons import response
+from rest_framework.decorators import api_view
+# Create your views here.
